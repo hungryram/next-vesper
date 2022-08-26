b8f5d5522ea0a92839ee9df11521e97c294751da
Ram Dettmer <ramdettmer751@gmail.com>
2022-08-26 13:33:40 -0700

    update

--- components/templates/BlogCard.js
+++ components/templates/BlogCard.js
@@ -4,24 +4,26 @@ import Image from "next/image"
 
 export default function BlogCard({ image, title, date, excerpt, link, altTag, _key }) {
     return (
-        <Link href={link} key={_key}>
-            <a>
-                <div className="md:flex gap-10 items-center border hover:shadow-md transition-all ease-linear">
-                    <div className="md:w-2/5 relative h-64 w-full">
-                        <Image
-                            src={urlFor(image).url()}
-                            layout="fill"
-                            objectFit="cover"
-                            alt={altTag}
-                        />
+        <div key={_key}>
+            <Link href={link}>
+                <a>
+                    <div className="md:flex gap-10 items-center border hover:shadow-md transition-all ease-linear">
+                        <div className="md:w-2/5 relative h-64 w-full">
+                            <Image
+                                src={urlFor(image).url()}
+                                layout="fill"
+                                objectFit="cover"
+                                alt={altTag}
+                            />
+                        </div>
+                        <div className="md:3/5">
+                            <h2 className="h3">{title}</h2>
+                            {date && <span>{date}</span>}
+                            {excerpt && <p className="mt-6">{excerpt}</p>}
+                        </div>
                     </div>
-                    <div className="md:3/5">
-                        <h2 className="h3">{title}</h2>
-                        {date && <span>{date}</span>}
-                        {excerpt && <p className="mt-6">{excerpt}</p>}
-                    </div>
-                </div>
-            </a>
-        </Link>
+                </a>
+            </Link>
+        </div>
     )
 }
