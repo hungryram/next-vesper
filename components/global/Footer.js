0dcfe3847bf19eb98ff969779643c3b96d217d26
Ram Dettmer <ramdettmer751@gmail.com>
2022-08-26 22:22:30 -0700

    update

--- components/global/Footer.js
+++ components/global/Footer.js
@@ -4,11 +4,22 @@ import useSWR from 'swr'
 
 export default function Footer() {
 
+    const fetcher = async (url) => await sanityRes.fetch(url)
+
+    const profile = groq`
+    *[_type == 'profileSettings'][0]
+    `
+
+
+    const { data, error } = useSWR(profile, fetcher);
+
+      if (error) return "An error has occurred.";
+      if (!data) return "Loading...";
 
     return (
         <footer className="text-center lg:text-left bg-gray-100 text-gray-600">
             <div className="container text-center">
-                <h1 className="text-8xl">Vesper</h1>
+                <h1 className="text-8xl">{data.company_name}</h1>
             </div>
         </footer>
     )
