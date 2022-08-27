c7b34bc379e704a1a18aa381d470fcaf36a4fed7
Ram Dettmer <ramdettmer751@gmail.com>
2022-08-26 23:53:33 -0700

    update

--- components/global/Footer.js
+++ components/global/Footer.js
@@ -6,7 +6,7 @@ import React from 'react'
 
 export default function Footer() {
 
-    const fetcher = (...args) => sanityRes.fetch(...args).then(res => res.json())
+    const fetcher = (...args) => sanityRes.fetch(...args)
 
 
     const profile = groq`
@@ -15,11 +15,11 @@ export default function Footer() {
     }
     `
     
-    const { data, error } = useSWR(`https://041e5s03.api.sanity.io/v2021-10-21/data/query/production?query=%20%20%20%20*%5B_type%20%3D%3D%20%27profileSettings%27%5D%5B0%5D`, fetcher);
+    const { data, error } = useSWR(groq`*[_type == "profileSettings"][0]`, fetcher);
 
       if (error) return "An error has occurred.";
       if (!data) return "Loading...";
-
+console.log(data)
     return (
         <footer className="text-center lg:text-left bg-gray-100 text-gray-600">
             <div className="container text-center">
