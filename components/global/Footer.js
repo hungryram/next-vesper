73e0903f6c97a281edb54068272d2626c024dd4e
Ram Dettmer <ramdettmer751@gmail.com>
2022-08-27 11:55:37 -0700

    update

--- components/global/Footer.js
+++ components/global/Footer.js
@@ -7,19 +7,17 @@ import React from 'react'
 export default function Footer() {
 
     const fetcher = (...args) => sanityRes.fetch(...args)
-
-
     const profile = groq`
     *[_type == 'profileSettings'][0]{
         company_name
     }
     `
     
-    const { data, error } = useSWR(groq`*[_type == "profileSettings"][0]`, fetcher);
+    const { data, error } = useSWR(profile, fetcher);
 
-      if (error) return "An error has occurred.";
-      if (!data) return "Loading...";
-console.log(data)
+    if (error) return "An error has occurred.";
+    if (!data) return "Loading...";
+
     return (
         <footer className="text-center lg:text-left bg-gray-100 text-gray-600">
             <div className="container text-center">
