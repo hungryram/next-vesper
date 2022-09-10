d38d96b8f3935af492c9a4a64ec721290e9ea569
Ram Dettmer <ramdettmer751@gmail.com>
2022-09-09 21:56:06 -0700

    update

--- pages/ihf.js
+++ pages/ihf.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Ihf() {
   return (
     <div>
-      <div dangerouslySetInnerHTML={{ __html: `{idx_body}` }}/>
+      <div dangerouslySetInnerHTML={{ __html: "{idx_body}" }}/>
     </div>
   )
 }
