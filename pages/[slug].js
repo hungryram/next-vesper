5f99a11c50894b08e54711c3aab2dc7f2a0ebc4e
Ram Dettmer <ramdettmer751@gmail.com>
2022-09-09 17:05:28 -0700

    delete idx

--- pages/[slug].js
+++ pages/[slug].js
@@ -174,11 +174,11 @@ export default function Pages({ page }) {
                                                         dangerouslySetInnerHTML={{ __html: `${section?.code}` }}
                                                     />
                                                 </div>
-                                                <script type="text/javascript"
-                                                    src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
-                                                <script type="text/javascript">
-                                                    jQuery.noConflict();
-                                                </script>
+                                                <Script
+                                                    src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js" />
+                                                <div
+                                                    dangerouslySetInnerHTML={{ __html: `<script>jQuery.noConflict();</script>` }}
+                                                />
                                             </div>
                                         )
                                     }
