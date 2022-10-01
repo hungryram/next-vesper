52f049641310596c52660f5f159f3016e94712f6
Brad S <94940930+brad-adrenalize@users.noreply.github.com>
2022-10-01 15:39:39 -0600

    edit postmark api function

--- pages/api/postmark.js
+++ pages/api/postmark.js
@@ -3,8 +3,7 @@ import { ServerClient } from 'postmark'
 export default async function (req, res) {
   const serverToken = process.env.NEXT_PUBLIC_POSTMARK_API_TOKEN;
   const client = new ServerClient(serverToken);
-  return new Promise(resolve => {
-    client.sendEmailWithTemplate({
+  const submit = await client.sendEmailWithTemplate({
       "From": process.env.NEXT_PUBLIC_POSTMARK_EMAIL,
       "To": process.env.NEXT_PUBLIC_POSTMARK_EMAIL,
       "ReplyTo": req.body.email,
@@ -14,21 +13,15 @@ export default async function (req, res) {
         "email": req.body.email,
         "phone": req.body.phone,
         "message": req.body.message
-      }}).then(function(res) {
-        if(res.ErrorCode === 0){
-          // Add backend action for successful form submission or remove
-          console.log('API: Form Submitted')
-        }
-        else {
-          // Add backend action for unsuccessful form submission or remove
-          console.log('API: Error With Form Submission')
-        }
-    }, function(err) {
-        // Do something with error response or remove
-        console.error(err)
-    });
-    res.end();
-    return resolve()
+      }}.then(function(res) {
+    return res
+  }, function(err) {
+    return err
   })
+    res.end()
+    return submit
 }
+
+
+
 
