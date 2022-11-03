8d5fe2d2ad15a9e4d8e597d5cf838a21bd7a3618
Ram Dettmer <ramdettmer751@gmail.com>
2022-11-03 13:44:08 -0700

    ihf fix

---
+++ pages/new.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function New({ idx_body }) {
+  return (
+    <div className="section">
+        <h1>IHOMEFINDER</h1>
+        {idx_body}
+    </div>
+  )
+}
