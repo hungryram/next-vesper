b6c4e6e645617c360f116daa8987a8333b895cec
Ram Dettmer <ramdettmer751@gmail.com>
2022-11-03 16:06:10 -0700

    update

--- pages/new.js
+++ pages/new.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function New({ idx_body }) {
   return (
     <div className="section">
-        <h1>IHOMEFINDER</h1>
+        <h1>IHOMEFINDER 2</h1>
         <div
             dangerouslySetInnerHTML={{
                 __html: `<div>{idx_body}</div>`
